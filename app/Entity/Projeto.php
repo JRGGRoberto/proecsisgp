4e747a5c3c274bc7500c541dd53bb34eb612b450
Roberto <jrggroberto@gmail.com>
2023-03-01 15:23:12 -0300

    Acerto no Projetos Entity - Submeter

--- app/Entity/Projeto.php
+++ app/Entity/Projeto.php
@@ -244,9 +244,9 @@ class Projeto{
     $this->atualizar();
 
     $sql = "
-          insert into 
-            avaliacoes (id, id_proj, ver, regra_def, fase_seq, tp_instancia, id_instancia)
-          select * 
+          insert into
+             avaliacoes ( id, id_proj, ver, regra_def, fase_seq, tp_instancia, id_instancia, resultado )
+             select id, id_proj, ver, regra_def, fase_seq, tp_avaliador, id_instancia, if(last_result='n', 'e', last_result)  
           from to_avaliar 
           where 
             id_proj = '". $this->id ."' and
