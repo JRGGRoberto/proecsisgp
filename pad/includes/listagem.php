6fa6b604a79d7dec11f5e45e47822e2ffef1f7e4
Roberto <jrggroberto@gmail.com>
2023-10-09 16:24:56 -0300

    :test_tube: PAD - testes iniciais 2

--- pad/includes/listagem.php
+++ pad/includes/listagem.php
@@ -2,61 +2,61 @@
 
 
 
-<div class="card mt-2">
-    <div class="card-header">
-       <strong>1. DADOS DO DOCENTE</strong>
-    </div>
-    <div id="dadosDoc" class="collapse show" data-parent="#accordion" style="">
-        <div class="card-body">
+    <div class="card mt-2">
+        <div class="card-header">
+            <strong>1. DADOS DO DOCENTE</strong>
+        </div>
+        <div id="dadosDoc" class="collapse show" data-parent="#accordion" style="">
+            <div class="card-body">
 
-        <table>
-  
-  <tr>
-    <th>Ano letivo</th>
-    <td colspan="3">2023</td>
+                <table class="table table-bordered table-sm">
 
-  </tr>
-  <tr>
-    <th>Nome</th>
-    <td></td>
-    <th>Regime de Trabalho</th>
-    <td></td>
-  </tr>
+                    <tr>
+                        <th>Ano letivo</th>
+                        <td colspan="3">2023</td>
 
-  <tr>
-    <th>Campus</th>
-    <td></td>
-    <th>Centro de área</th>
-    <td></td>
-  </tr>
-  
-  <tr>
-    <th>Colegiado</th>
-    <td></td>
-    <th>Área de concurso</th>
-    <td></td>
-  </tr>
-  
-  <tr>
-    <th>Maior Titulação</th>
-    <td></td>
-    <th>Data de obtenção do título</th>
-    <td></td>
-  </tr>
-  
-  <tr>
-    <th>Tempo de docência nos
-componentes curriculares</th>
-    <td></td>
-    <th>Tempo efetivo de docência no
-ensino superior na UNESPAR</th>
-    <td></td>
-  </tr>
-</table>
+                    </tr>
+                    <tr>
+                        <th>Nome</th>
+                        <td></td>
+                        <th>Regime de Trabalho</th>
+                        <td></td>
+                    </tr>
 
+                    <tr>
+                        <th>Campus</th>
+                        <td></td>
+                        <th>Centro de área</th>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <th>Colegiado</th>
+                        <td></td>
+                        <th>Área de concurso</th>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <th>Maior Titulação</th>
+                        <td></td>
+                        <th>Data de obtenção do título</th>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <th>Tempo de docência nos
+                            componentes curriculares</th>
+                        <td></td>
+                        <th>Tempo efetivo de docência no
+                            ensino superior na UNESPAR</th>
+                        <td></td>
+                    </tr>
+                </table>
+
+            </div>
         </div>
     </div>
-</div>
 
 
 
@@ -68,20 +68,32 @@ ensino superior na UNESPAR</th>
                     <div class="col-sm"><a class="card-link collapsed" data-toggle="collapse" href="#ativ21" aria-expanded="false"><strong>2.1. Atividades Didáticas</strong></a></div>
                 </div>
                 <div class="row">
-                   <div class="col-sm d-flex justify-content-end">Média semanal anual da carga horária didática: <strong> 0</strong></div>
+                    <div class="col-sm d-flex justify-content-end">Média semanal anual da carga horária didática: <strong> 0</strong></div>
                 </div>
             </div>
 
             <div id="ativ21" class="collapse" data-parent="#accordion" style="">
                 <div class="card-body">
 
-                teste
+                    <table id="tabela-equipe" class="table table-bordered table-sm">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>N</th>
+                                <th>Nome</th>
+                                <th>Instituição</th>
+                                <th>Formação</th>
+                                <th>Função na equipe</th>
+                                <th>Telefone</th>
+                                <th style="width:20px"><button type="button" class="btn btn-primary btn-sm" onclick="formAddEquipe()">Adicionar</button></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
 
                 </div>
             </div>
         </div>
-
-
 
 
         <div class="card mt-2">
@@ -96,7 +108,23 @@ ensino superior na UNESPAR</th>
 
             <div id="ativ22" class="collapse" data-parent="#accordion" style="">
                 <div class="card-body">
-                teste
+                    <table id="tabela-equipe" class="table table-bordered table-sm">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>Atividade</th>
+                                <th>Nome do(a) estudante</th>
+                                <th>Curso</th>
+                                <th>Série</th>
+                                <th>C H semanal(1o sem)</th>
+                                <th>C H semanal(2o sem)</th>
+                                <th style="width:20px"><button type="button" class="btn btn-primary btn-sm" onclick="formAddEquipe()">Adicionar</button></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+
+
                 </div>
             </div>
         </div>
@@ -111,13 +139,13 @@ ensino superior na UNESPAR</th>
                     <div class="col-sm"><a class="card-link" data-toggle="collapse" href="#ativ3" aria-expanded="false"><strong>3. ATIVIDADES DE PESQUISA / EXTENSÃO / CULTURA E PROGRAMAS ESPECIAIS</strong></a></div>
                 </div>
                 <div class="row">
-                   <div class="col-sm d-flex justify-content-end">Total de carga horária semanal pesquisa/extensão/cultura/programas especiais: <strong> 0</strong></div>
+                    <div class="col-sm d-flex justify-content-end">Total de carga horária semanal pesquisa/extensão/cultura/programas especiais: <strong> 0</strong></div>
                 </div>
             </div>
 
             <div id="ativ3" class="collapse" data-parent="#accordion" style="">
                 <div class="card-body">
-                teste
+                    teste
 
                 </div>
             </div>
@@ -131,14 +159,14 @@ ensino superior na UNESPAR</th>
                     <div class="col-sm"><a class="card-link collapsed" data-toggle="collapse" href="#ativ4" aria-expanded="false"><strong>4. ATIVIDADES DE GESTÃO INSTITUCIONAL</strong></a></div>
                 </div>
                 <div class="row">
-                  <div class="col-sm d-flex justify-content-end">Total de carga horária semanal de gestão institucional:  <strong> 0</strong></div>
+                    <div class="col-sm d-flex justify-content-end">Total de carga horária semanal de gestão institucional: <strong> 0</strong></div>
                 </div>
             </div>
 
             <div id="ativ4" class="collapse" data-parent="#accordion" style="">
                 <div class="card-body">
 
-                teste
+                    teste
 
                 </div>
             </div>
@@ -148,25 +176,92 @@ ensino superior na UNESPAR</th>
 
 
     <div class="card mt-2">
-    <div class="card-header">
-       <strong>5. RESUMO DAS ATIVIDES E TOTALIZAÇÃO</strong>
-    </div>
-    <div id="resumo" class="collapse show" data-parent="#accordion" style="">
-        <div class="card-body">
-        Ano letivo: 2023
-Nome Virgínia Maria Nuss Regime de Trabalho: Integral (40 h)
-Campus: Apucarana Centro de Área: CCHE
-Colegiado: Letras Português Área de concurso:
-Maior Titulação: Data de obtenção do título:
-Tempo de docência nos
-componentes curriculares:
+        <div class="card-header">
+            <strong>5. RESUMO DAS ATIVIDES E TOTALIZAÇÃO</strong>
+        </div>
+        <div id="resumo" class="collapse show" data-parent="#accordion" style="">
+            <div class="card-body">
+                <table class="table table-bordered table-sm">
 
-Tempo efetivo de docência no
-ensino superior na UNESPAR:
+                    <tr>
+                        <th>1</th>
+                        <th>Total de média semanal anual de carga horária didática</th>
+                        <td>0h</td>
+                    <tr>
+                        <th>2</th>
+                        <th>Total de média semanal anual de carga horária supervisão e orientação</th>
+                        <td>0h</td>
+                    <tr>
+                        <th>3</th>
+                        <th>Total de carga horária semanal pesquisa/extensão/cultura/programas especiais</th>
+                        <td>0h</td>
+                    <tr>
+                        <th>4</th>
+                        <th>Total de carga horária semanal de gestão institucional</th>
+                        <td>0h</td>
+                    </tr>
 
+                    <tr>
+                        <th>4</th>
+                        <th>Total de carga horária semanal de gestão institucional</th>
+                        <td>0h</td>
+                    </tr>
+
+                    <tr>
+                        <th colspan="2">Total de carga horária semanal</th>
+                        <td>0h</td>
+                    </tr>
+
+
+                </table>
+
+            </div>
         </div>
     </div>
-</div>
+
+
+    <div class="card mt-2">
+        <div class="card-header">
+            <strong>6. OUTRAS OBSERVAÇÕES</strong>
+        </div>
+        <div id="resumo" class="collapse show" data-parent="#accordion" style="">
+            <div class="card-body">
+                <input type="text" name="" id="">
+            </div>
+        </div>
+    </div>
+
+
+
 
 </section>
 
+
+
+
+<!-- The Modal -->
+<div class="modal fade" id="modalSub">
+    <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title" id="modalTitle">Título</h4>
+          <button type="button" class="close" data-dismiss="modal">×</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body" id="modalBody">
+          
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer" id="modalFooter">
+          
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  <!-- The Modal -->
