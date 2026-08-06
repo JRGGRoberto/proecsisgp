0c8c8537227e9e152df6b6f41ed5c035373401a1
Roberto <jrggroberto@gmail.com>
2026-08-06 11:02:14 -0300

    Acertando filtros 2

--- avaliacoes/includes/data/datarel1.php
+++ avaliacoes/includes/data/datarel1.php
@@ -15,19 +15,15 @@ switch ($user['config']) {
         break;
     case 1: // Coordenador;
         array_push($condicoes,
-            '( ( tp_instancia = "co" and  id_instancia = "'.$user['co_id'].'"
-                  and exists (
-                                 select 1 
-                                 from 
-                                    colegiados c  
-                                 where 
-                                    c.id = id_instancia and
-                                    c.coord_id =  "'.$user['id'].'"
-                              )
+            '( 
+               ( tp_instancia = "co" and
+                 id_instancia = (select c.id from colegiados c where c.coord_id   = "'.$user['id'].'")  
                ) 
-                or 
+             or 
                ( tp_instancia = "pf" and  id_instancia = "'.$user['id'].'")   
-             )');
+             )
+               '
+        );
         break;
     case 2: // Dir Centro de Area CE
         array_push($condicoes,
